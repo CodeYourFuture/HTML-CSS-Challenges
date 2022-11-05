90d642227e2af960216607941a50f13c060fcfe1
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-11-05 23:12:31 +0000

    Added container with checking color into  index.html

--- script.js
+++ script.js
@@ -6,5 +6,24 @@ function showMessage(event) {
   submitedForm.innerHTML = `Thank you for your order, ${firstName} ${lastName}! We will send your order as soon as possible. Have a nice day!`;
 }
 
-var submitedForm = document.querySelector(".order-form");
+//listening to submiting form
+let submitedForm = document.querySelector(".order-form");
 submitedForm.addEventListener("submit", showMessage);
+
+function changeIntoGreen(event) {
+  event.preventDefault();
+  alert("Green");
+}
+
+function changeIntoBrown(event) {
+  event.preventDefault();
+  alert("Brown");
+}
+
+let greenColor = document.getElementById("green-t-shirt");
+console.log(greenColor);
+greenColor.addEventListener("click", changeIntoGreen);
+
+let brownColor = document.getElementById("brown-t-shirt");
+console.log(brownColor);
+brownColor.addEventListener("click", changeIntoBrown);

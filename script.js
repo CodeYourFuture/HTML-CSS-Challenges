a3ea5018416b2f0d7e01c4d1f27dc3f6792ee732
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-11-06 11:36:47 +0000

    Styling & adding function to color picker & image slider

--- script.js
+++ script.js
@@ -10,20 +10,77 @@ function showMessage(event) {
 let submitedForm = document.querySelector(".order-form");
 submitedForm.addEventListener("submit", showMessage);
 
+//changing images on image slider depending on color
+var greenLine = document.getElementById("green_line");
+var brownLine = document.getElementById("brown_line");
+var whiteLine = document.getElementById("white_line");
+
 function changeIntoGreen(event) {
   event.preventDefault();
-  alert("Green");
+  document.querySelector(".box-1").style.backgroundImage =
+    "url('media/green_1.jpg')";
+  document.querySelector(".box-2").style.backgroundImage =
+    "url('media/green_2.jpg')";
+  document.querySelector(".box-3").style.backgroundImage =
+    "url('media/green_3.jpg')";
+  document.querySelector(".box-4").style.backgroundImage =
+    "url('media/green_4.jpg')";
+
+  //removing line from the color
+  brownLine.classList.remove("clicked");
+  whiteLine.classList.remove("clicked");
+  if (greenLine.classList.contains("clicked")) {
+  } else {
+    greenLine.classList.add("clicked");
+  }
+  //   greenColor.classList.add("clicked");
 }
 
 function changeIntoBrown(event) {
   event.preventDefault();
-  alert("Brown");
+  document.querySelector(".box-1").style.backgroundImage =
+    "url('media/brown_1.jpg')";
+  document.querySelector(".box-2").style.backgroundImage =
+    "url('media/brown_2.jpg')";
+  document.querySelector(".box-3").style.backgroundImage =
+    "url('media/brown_3.jpg')";
+  document.querySelector(".box-4").style.backgroundImage =
+    "url('media/brown_4.jpg')";
+
+  //removing/adding line from the color
+  greenLine.classList.remove("clicked");
+  whiteLine.classList.remove("clicked");
+  if (brownLine.classList.contains("clicked")) {
+  } else {
+    brownLine.classList.add("clicked");
+  }
+}
+
+function changeIntoWhite(event) {
+  event.preventDefault();
+  document.querySelector(".box-1").style.backgroundImage =
+    "url('media/white_1.jpg')";
+  document.querySelector(".box-2").style.backgroundImage =
+    "url('media/white_2.jpg')";
+  document.querySelector(".box-3").style.backgroundImage =
+    "url('media/white_3.jpg')";
+  document.querySelector(".box-4").style.backgroundImage =
+    "url('media/white_4.jpg')";
+
+  //removing line from the color
+  greenLine.classList.remove("clicked");
+  brownLine.classList.remove("clicked");
+  if (whiteLine.classList.contains("clicked")) {
+  } else {
+    whiteLine.classList.add("clicked");
+  }
 }
 
 let greenColor = document.getElementById("green-t-shirt");
-console.log(greenColor);
 greenColor.addEventListener("click", changeIntoGreen);
 
 let brownColor = document.getElementById("brown-t-shirt");
-console.log(brownColor);
 brownColor.addEventListener("click", changeIntoBrown);
+
+let whiteColor = document.getElementById("white-t-shirt");
+whiteColor.addEventListener("click", changeIntoWhite);
